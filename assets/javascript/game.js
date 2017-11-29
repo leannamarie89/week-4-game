f9c0e73c01f28f0813f573fe4c199f402313a395
Leanna Smith <leannasmith@Leannas-Air.home>
2017-11-28 19:31:44 -0500

    week-4-game

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+var random_result;
+var lost; 
+var win; 
+
+
+for (var i = 0; i < 4; i++){
+	var crystal = $("<div>");
+	    crystal.attri("class")
+
+	$(".crystals").append(crystal);
+	console.log()
+}
